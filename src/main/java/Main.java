de964e3fa9ec3c9b261d78e63552b26c01073631
natalliakharchenko <morozko.nnn@gmail.com>
2023-10-17 09:21:37 +0200

    Added check with book that has good email

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,14 +11,24 @@ public class Main {
         Book myBook = new Book();
         myBook.setIsbn("ISBN-1-56619-909-3");
         System.out.println("Has book valid isbn: " + myBook.isValid());
+        System.out.println("-------------------");
 
         //Solution with Jakarta annotations
         ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
         Validator validator = factory.getValidator();
 
+        System.out.println("Book2 validation: ");
         BookWithJakartaAnnotations book2 = new BookWithJakartaAnnotations("001-56619-909-3", "bhla");
         Set<ConstraintViolation<BookWithJakartaAnnotations>> violations = validator.validate(book2);
         for (ConstraintViolation<BookWithJakartaAnnotations> violation : violations) {
+            System.out.println(violation.getMessage());
+        }
+
+        System.out.println("-------------------");
+        System.out.println("Book3 validation: ");
+        BookWithJakartaAnnotations book3 = new BookWithJakartaAnnotations("1-56619-909-3", "info@gmail.com");
+        Set<ConstraintViolation<BookWithJakartaAnnotations>> book3Violations = validator.validate(book3);
+        for (ConstraintViolation<BookWithJakartaAnnotations> violation : book3Violations) {
             System.out.println(violation.getMessage());
         }
 
